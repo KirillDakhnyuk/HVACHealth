05d7812afcf21d2557d87dc898b67a7aec933132
Kirill Dakhnyuk <kirilldakhnyuk@gmail.com>
2022-06-29 17:21:27 +0300

    Update RunHealthMonitorsCommand.php

--- src/Commands/RunHealthMonitorsCommand.php
+++ src/Commands/RunHealthMonitorsCommand.php
@@ -50,7 +50,7 @@ class RunHealthMonitorsCommand extends Command
             ->each(fn (Result $result) => DB::connection(config('hvac-health.connection'))
             ->table(config('hvac-health.table'))->insert([
                 'project' => config('hvac-health.project'),
-                'check_name' => $result->name,
+                'name' => $result->name,
                 'meta' => collect($result->meta),
                 'status' => $result->status->value,
                 'notification_message' => $result->notificationMessage,
